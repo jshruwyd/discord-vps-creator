fb277ffe55982357d10db8e24323c1e086a6a0a3
jshruwyd <159998785+jshruwyd@users.noreply.github.com>
2024-11-23 04:58:06 -0800

    Update b.py

--- b.py
+++ b.py
@@ -1,12 +1,13 @@
 import gdown
 
-# URL file Google Drive
-url = "https://drive.google.com/file/d/1WlzOMBtITNea7X8Qu-EIo0rErRq4SAg_"
+# URL file Google Drive (thay đổi từ dạng "view" sang "uc" để gdown hoạt động)
+url = "https://drive.google.com/uc?id=11reY6uzf0BdTLLbCj_ecs0qDxWFaLTEx"
 
 # Đường dẫn lưu file sau khi tải
-output = "/mnt/win11.zip"  # Đổi tên file tùy ý
+output = "/mnt/win11.zip"  # Đường dẫn đầy đủ nơi lưu file
 
 # Tải file
 gdown.download(url, output, quiet=False)
 
 print(f"File đã được tải về và lưu tại: {output}")
+
